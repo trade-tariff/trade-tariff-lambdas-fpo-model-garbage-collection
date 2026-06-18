8e9ea8a4e80478f2c017a1e423a2cd6554dc0f63
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:14:22 +0100

    chore: manage pre-commit hooks with Nix
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,12 +2,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { system = system; };
+        pkgs = import nixpkgs { inherit system; };
 
         lint = pkgs.writeScriptBin "lint" ''
           pre-commit run --all-files --show-diff-on-failure
@@ -16,17 +26,97 @@
           cd collector
           go get -u ./...
         '';
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            circleci-cli
-            go
-            golangci-lint
-            gopls
-            lint
-            serverless
-            update-modules
-          ];
+
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            actionlint = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-added-large-files = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-case-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-merge-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            detect-private-keys = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            markdownlint = {
+              enable = true;
+              excludes = [ "^terraform/" ];
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            statix = {
+              enable = true;
+              settings.ignore = [ ".direnv" ];
+              stages = [ "pre-commit" ];
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            trufflehog = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+
+            golangci-lint = {
+              enable = true;
+              name = "golangci-lint";
+              description = "Run golangci-lint in collector";
+              entry = "bash -c 'cd collector && golangci-lint run ./...'";
+              files = "^collector/.*\\.go$";
+              pass_filenames = false;
+              stages = [ "pre-commit" ];
+            };
+          };
         };
-      });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${preCommitCheck.shellHook}
+          '';
+
+          buildInputs =
+            preCommitCheck.enabledPackages
+            ++ (with pkgs; [
+              circleci-cli
+              go
+              golangci-lint
+              gopls
+              lint
+              serverless
+              update-modules
+            ]);
+        };
+      }
+    );
 }
